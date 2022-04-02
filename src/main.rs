c0ac0fbc47e7f33bab656ac5457beccaace448ce
FreePlacki <jk216635@gmail.com>
2022-04-02 12:35:31 +0200

    added sub-tasks and task info printing

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ impl Task {
         }
     }
 
-    fn print_task(&self) {
+    fn print_header(&self) {
         match self.importance {
             3 => print!("\x1b[1;37;31m{}\x1b[0m", self.title),
             2 => print!("\x1b[1;37;33m{}\x1b[0m", self.title),
@@ -47,10 +47,39 @@ impl Task {
 
             println!("\t\x1b[1;37;30m [due in {}]\x1b[0m", time_left);
         }
+    }
+
+    fn print_task(&self) {
+        self.print_header();
 
         if self.description != "" {
             println!("{}", self.description);
         }
+
+        if !self.sub_tasks.is_empty() {
+            println!("\x1b[1;37;37mSub tasks ({}):\x1b[0m", self.sub_tasks.len());
+            for task in &self.sub_tasks {
+                task.print_header();
+            }
+        }
+    }
+
+    fn print_info(&self) {
+        println!("\x1b[1;37;37mTitle:\x1b[0m {}", self.title);
+        println!("\x1b[1;37;37mDescription:\x1b[0m {}", self.description);
+        println!("\x1b[1;37;37mImportance:\x1b[0m {}", self.importance);
+        println!(
+            "\x1b[1;37;37mDate created:\x1b[0m {}",
+            self.date_created.format("%Y-%m-%d %H:%M:%S")
+        );
+        println!(
+            "\x1b[1;37;37mDate due:\x1b[0m {}",
+            if let Some(due_date) = self.due_date {
+                due_date.format("%Y-%m-%d %H:%M:%S").to_string()
+            } else {
+                "None".to_string()
+            }
+        );
     }
 
     fn add_sub_task(&mut self, sub_task: Task) {
@@ -59,13 +88,21 @@ impl Task {
 }
 
 fn main() {
-    let task = Task::new(
+    let mut task = Task::new(
         "Task 1".to_string(),
         "This is a task".to_string(),
         1,
         Some(Local.ymd(2022, 4, 17).and_hms(0, 0, 0)),
     );
+    task.add_sub_task(Task::new(
+        "Sub task 1".to_string(),
+        "This is a sub task".to_string(),
+        2,
+        Some(Local.ymd(2022, 4, 17).and_hms(0, 0, 0)),
+    ));
 
     task.print_task();
-    println!("created {} ago", date::get_time_left(task.date_created));
+    println!();
+    task.print_info();
+    task.sub_tasks[0].print_info();
 }
